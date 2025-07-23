a5015ed4d945134703879e0f555e7a8688994cb5
Raghavendra Kulkarni <kulkarni15raghav@gmail.com>
2025-07-23 18:32:55 +0530

    added some more fields to the database

--- db-hackathon/backend/services/farmer_service.py
+++ db-hackathon/backend/services/farmer_service.py
@@ -11,7 +11,25 @@ class FarmerService:
             name=data['name'],
             email=data['email'],
             phone=data['phone'],
+            wallet_address=1234567890,
+            contract_address=data['contract_address'],
+            password_hash=data['password_hash'],
+            total_loans=0,
+            total_loans_repaid=0,
+            total_on_time_loans_repaid=0,
+            total_loans_remaining=0,
+            total_loans_defaulted=0,
+            average_interest_rate=0,
+            confidence_score=0,
+            state=data['state'],
+            city=data['city']
         )
-
+        return self.farmer_repository.add_farmer(farmer)
+    
+    def get_farmer_by_id(self, aadhar_id: str):
+        return self.farmer_repository.get_farmer_by_id(aadhar_id)
+    
+    def get_all_farmers(self):
+        return self.farmer_repository.get_all_farmers()
 
     
--- db-hackathon/backend/services/transaction_service.py
+++ db-hackathon/backend/services/transaction_service.py
@@ -1,5 +1,6 @@
 from backend.repositories.transaction_repository import TransactionRepository
 from backend.database import Transaction
+from datetime import datetime
 
 class TransactionService:
     def __init__(self):
@@ -7,9 +8,22 @@ class TransactionService:
 
     def create_transaction(self, data):
         transaction = Transaction(
-            project_id=data['project_id'],
+            transaction_id=1,
+            farmer_aadhar_id=data['farmer_aadhar_id'],
+            investor_aadhar_id=data['investor_aadhar_id'],
             amount=data['amount'],
-            transaction_type=data['transaction_type'],
-            transaction_date=data['transaction_date']
+            project_id=data['project_id'],
+            status="completed",
+            timestamp=datetime.now()
         )
         return self.transaction_repository.add_transaction(transaction)
+
+    def get_transactions_by_farmer_aadhar_id(self, farmer_aadhar_id: str):
+        return self.transaction_repository.get_transactions_by_farmer_aadhar_id(farmer_aadhar_id)
+    
+    def get_transactions_by_investor_aadhar_id(self, investor_aadhar_id: str):
+        return self.transaction_repository.get_transactions_by_investor_aadhar_id(investor_aadhar_id)
+    
+    def get_transactions_by_project_id(self, project_id: int):
+        return self.transaction_repository.get_transactions_by_project_id(project_id)
+    
